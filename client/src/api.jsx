82f25d7857114863ce8bfb7e602245799e3bce54
Riya Tripathi <tripathiriya2117@gmail.com>
2026-06-20 01:21:56 -0700

    fix:url

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,2 +1,2 @@
-const BASE_URL = import.meta.env.VITE_API_URL?.replace(/\/$/, '')|| 'http://localhost:5000'
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://period-tracker-api-tfld.onrender.com'
 export default BASE_URL
